af807eb06a1f766602739d58f07a3b1c9cde07be
Guillaume Lelarge <guillaume@lelarge.info>
2012-03-31 15:53:55 +0200

    Add more informations on PLpgsql plugin hooks, and an example

---
+++ examples/logplpgsql/logplpgsql.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "postgres.h"
+#include "plpgsql.h"
+
+PG_MODULE_MAGIC;
+
+void load_plugin( PLpgSQL_plugin * hooks );
+static void profiler_func_beg(PLpgSQL_execstate * estate, PLpgSQL_function * func);
+
+static PLpgSQL_plugin plugin_funcs = { profiler_func_beg };
+
+void _PG_init( void )
+{
+	PLpgSQL_plugin ** var_ptr = (PLpgSQL_plugin **) find_rendezvous_variable("PLpgSQL_plugin");
+
+	*var_ptr = &plugin_funcs;
+}
+
+void load_plugin( PLpgSQL_plugin * hooks )
+{
+	hooks->func_beg   = profiler_func_beg;
+}
+
+static void profiler_func_beg( PLpgSQL_execstate * estate, PLpgSQL_function * func )
+{
+	elog(LOG, "Execute function %s", func->fn_name);
+}
